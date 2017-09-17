5f7f0cb0a4a2e83b63ffbc1a0cbe3ec7db496860
unknown <andi.iv@abv.bg>
2017-09-17 19:40:05 +0300

    Generics lab added to github

---
+++ Problem.1 Box/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+public class Box<T>
+{
+    private List<T> data;
+
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public int Count => data.Count;
+
+    public void Add(T element)
+    {
+        this.data.Add(element);
+    }
+
+    public T Remove()
+    {
+        var element = this.data[data.Count - 1];
+        this.data.RemoveAt(data.Count-1);
+
+        return element;
+    }
+
+}
+
---
+++ Problem.1 Box/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Box<int> box = new Box<int>();
+        box.Add(1);
+        box.Add(2);
+        box.Add(3);
+        Console.WriteLine(box.Remove());
+        Console.WriteLine(box.Count);
+    }
+}
+
